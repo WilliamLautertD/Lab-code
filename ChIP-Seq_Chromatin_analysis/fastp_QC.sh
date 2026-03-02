b8859c2b9108d7c25f4b59cc8664c005087691e2
William Lautert Dutra, MSc <79327373+WilliamLautertD@users.noreply.github.com>
2026-03-02 13:23:12 -0500

    Modify fastp_QC.sh for new data directory and commands
    Updated paths and commands for QC and trimming processes.

--- ChIP-Seq_Chromatin_analysis/fastp_QC.sh
+++ ChIP-Seq_Chromatin_analysis/fastp_QC.sh
@@ -1,24 +1,60 @@
+#!/bin/bash
+#SBATCH -p general
+#SBATCH --job-name=QC_and_trimming
+#SBATCH --output=QC_and_trimming.log
+#SBATCH --error=QC_and_trimming.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=5
+#SBATCH --cpus-per-task=20
+#SBATCH --time=72:00:00
+#SBATCH --mail-user=William.LautertDutra@fccc.edu
+#SBATCH --mail-type=END,FAIL
+
+# Load modules (if needed)
+# module load fastp
+# module load FastQC
+# module load MultiQC
+
 # Define constants
-DATADIR="/Users/williamlautert/Desktop/Whetstine_Lab/MapR"
-LIST="${DATADIR}/data/basenames.txt"
+DATADIR="/home/lauterw/RPE_Takara_Chip_Seq_Test/02_26_2026_RPE_Takara_ChIP_Seq_Test_3/data"
+LIST="${DATADIR}/filenames.txt"
 
-OUTDIR_trimmed="${DATADIR}/data/trimmed"
-OUTDIR_qc="${DATADIR}/results/"
+OUTDIR="${DATADIR}/trimmed"
 
-export REF DATADIR LIST OUTDIR_trimmed OUTDIR_qc
+# Make output directory if it doesn't exist
+mkdir -p $OUTDIR
+mkdir -p $OUTDIR/qc
 
-# Running fastp
-parallel -j 1 '
-    fastp --in1 "${DATADIR}/data/{}_R1_001.fastq.gz" --in2 "${DATADIR}/data/{}_R2_001.fastq.gz" \
-    --out1 "${OUTDIR_trimmed}/{}_trimmed_R1.fastq.gz" \
-    --out2 "${OUTDIR_trimmed}/{}_trimmed_R2.fastq.gz" \
-    --thread 8
+export DATADIR LIST OUTDIR
+
+# Running FASTQC on raw reads
+parallel -j 5 --dry-run '
+    echo fastqc "${DATADIR}/{}_L001_R1_001.fastq.gz" -o ${DATADIR}/qc/
+    conda run qc_analysis fastqc \
+        "${DATADIR}/{}_L001_R1_001.fastq.gz" \
+        -o ${DATADIR}/qc/ -t 20
+    
+    echo fastqc "${DATADIR}/{}_L001_R2_001.fastq.gz" -o ${DATADIR}/qc/
+    conda run qc_analysis fastqc \
+        "${DATADIR}/{}_L001_R2_001.fastq.gz" \
+        -o ${DATADIR}/qc/ -t 20
+' :::: $LIST
+
+# Running fastp for trimming
+parallel -j 5 --dry-run '
+    conda run qc_analysis fastp \
+    --in1 "${DATADIR}/{}_L001_R1_001.fastq.gz" \
+    --in2 "${DATADIR}/{}_L001_R2_001.fastq.gz" \
+    --out1 "${OUTDIR}/{}_trimmed_R1.fastq.gz" \
+    --out2 "${OUTDIR}/{}_trimmed_R2.fastq.gz" \
+    --thread 20
 ' :::: $LIST
 
 # QC report for trimmed reads
 # FastQC
-fastqc $OUTDIR_trimmed/* -o $OUTDIR_qc/fastqc/
+echo fastqc $OUTDIR/* -o $OUTDIR/qc/ -t 20
 
 # MultiQC
-multiqc $OUTDIR_qc/fastqc/ --outdir $OUTDIR_qc/multiqc \
-    --title "MultiQC Report - Trimmed Reads"
+echo multiqc $OUTDIR/qc/fastqc/ \
+    --outdir $OUTDIR/qc/multiqc \
+    --title "MultiQC Report"
